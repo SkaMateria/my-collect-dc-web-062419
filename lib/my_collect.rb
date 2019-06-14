df778b42ecae1af1c5b64da951630c7a8eb8e3fc
Carlos Velazquez <skamateria@gmail.com>
2019-06-14 19:44:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,9 @@
-def my_collect(array)
-  array = []
-  my_collect(array) do |up|
-    up.upcase
-  end
+def my_collect(collection)
+  new_collection = []
+  i = 0 
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end 
+  new_collection
   end
